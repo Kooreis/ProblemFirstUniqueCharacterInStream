ae1699dc1f2c87e1f58b2fd25e3b031b7601faef
Kooreis <kooreis.official@gmail.com>
2024-09-07 04:01:19 -0500

    The first part of the script sets up the main function of the program. It prompts the user to enter a string and reads the input from the console.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-Here is a simple console application in C# that finds the first unique character in a stream:
-
-```C#
 using System;
 using System.Collections.Generic;
 
@@ -10,36 +7,5 @@ class Program
     {
         Console.WriteLine("Enter a string:");
         string input = Console.ReadLine();
-        Console.WriteLine("First unique character: " + GetFirstUniqueCharacter(input));
-    }
-
-    static char GetFirstUniqueCharacter(string input)
-    {
-        Dictionary<char, int> characterCount = new Dictionary<char, int>();
-
-        foreach (char c in input)
-        {
-            if (characterCount.ContainsKey(c))
-            {
-                characterCount[c]++;
-            }
-            else
-            {
-                characterCount[c] = 1;
-            }
-        }
-
-        foreach (char c in input)
-        {
-            if (characterCount[c] == 1)
-            {
-                return c;
-            }
-        }
-
-        return '\0';
     }
 }
-```
-
-This program reads a string from the console, then uses a dictionary to count the occurrences of each character in the string. It then iterates over the string again, returning the first character that only appears once. If no such character exists, it returns the null character (`'\0'`).
